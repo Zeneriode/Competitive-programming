69352153112eff2036040371fc6ca1ae9e3cc25d
Zener_085 <zener.085@mail.ru>
2022-08-18 16:04:02 +0300

    Создан шаблон для олимпиадных задач
    Создан шаблон, который можно использовать, чтобы проверять задачи с тестовым вводом и выводом
    
    Настроены Clang и Cmake под работу над олимпиадными задачами

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// Copyright 2022 Zener
+// Шаблон для олимпиадных задач
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve();
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    /*is Single Test case?*/ cin >> t;
+    while (t--) {
+        solve();
+        cout << "\n";
+    }
+
+    cerr << "time taken : " << (float) clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
+
+void solve() {
+    cout << 10;
+}
